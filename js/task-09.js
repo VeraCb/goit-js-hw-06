2576b2ab5f9c0956a09633d84baa313873c9de89
VeraCB <bogosveronica98'gmail.com>
2024-04-04 22:48:01 +0100

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+var button = document.querySelector('.change-color');
+var colorSpan = document.querySelector('.color');
+
+
+button.addEventListener('click', function() {
+ 
+  var randomColor = getRandomHexColor();
+
+  
+  document.body.style.backgroundColor = randomColor;
+
+  colorSpan.textContent = randomColor;
+});
+
